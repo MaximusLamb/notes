727efd46a349fe529a89b465719e546ca6027cba
Max Lamb <maxlambcodes@gmail.com>
2020-06-04 17:14:30 -0700

    index working

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 const Input = require('./lib/Input');
-const Note = require('./lib/notes');
+const Note = require('./lib/NoteSchema');
+
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost:27017/notary', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
 
 let input = new Input(process.argv);
 
 const isValid = input.valid();
 // Input(notes) ? Note(notes) : console.log('Bad Input');
 
-isValid ? Note.execute(input) : console.log('Fail');
+isValid ? (Note.execute(input)) : console.log('Fail');
