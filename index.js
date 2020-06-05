6cac9bffbcff5992068af7e6c9a46afe088ff4c8
Max Lamb <maxlambcodes@gmail.com>
2020-06-04 17:29:10 -0700

    cleaned code

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 const Input = require('./lib/Input');
 const Note = require('./lib/notes');
 
-let input = new Input(process.argv);
+const input = new Input(process.argv);
 
 const isValid = input.valid();
-// Input(notes) ? Note(notes) : console.log('Bad Input');
 
 isValid ? Note.execute(input) : console.log('Fail');
