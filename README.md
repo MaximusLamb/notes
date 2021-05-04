a8070960d5cb404106099fa49ac5196b12a310fe
langstonBS <langston.beckwithstanley@gmail.com>
2021-05-04 13:17:09 -0700

    refacotrd to help jillian

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # notes
+
+
+### to run app type node index.js --add comand comand
+
+
--- index.js
+++ index.js
@@ -1,8 +1,9 @@
-const { parse, valid } = require('./input');
-const { execute } = require('./notes')
+const { parse, valid } = require('./lib/input.js');
+const { execute } = require('./lib/notes')
 const minimist = require('minimist');
 
 const notes = parse(process.argv);
+console.log(notes)
 
 valid(notes) ? execute(notes) : console.log('Bad Input');
 
